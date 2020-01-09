95b35922471c84c9e55a2fbbf36723d538e098b7
Csongor Keller <csongorkeller@Csongors-MacBook-Pro.local>
2020-01-09 21:05:41 +0100

    color constants added

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//App colors
+final primaryColor = brandLightBlue;
+final brandLightBlue = const Color.fromRGBO(
+    124,158,178, 1.0); //Greyish blue general background color
+final brandPurpleNavy = const Color.fromRGBO(82,82,140,1.0); // Pink
+
+final brandRussianViolet = const Color.fromRGBO(55,37,84,1.0);
+
+final brandRaisinBlack = const Color.fromRGBO(35,17,35,1.0);
+
+final brandRegistrationBlack = const Color.fromRGBO(0,0,0,1.0);
+
+
