16515baab7164f768726ef3f2c11119968d72818
Csongor Keller <csongorkeller@Csongors-MacBook-Pro.local>
2020-01-26 22:54:15 +0100

    Calculates daily sum of transactions for the past 7 days

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
 import './models/transaction.dart';
 import './utils/constants.dart';
+import 'widgets/chart.dart';
 
 void main() => runApp(MyApp());
 
@@ -39,6 +40,15 @@ class _MyHomePageState extends State<MyHomePage> {
       date: DateTime.now(),
     ), */
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7)
+          ));
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
@@ -92,14 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
